fe87ea3579769a98227dd044d0a105637bebf381
henri <henri.shustak@gmail.com>
2022-11-09 15:49:00 +1300

    Update osx_alias.bash

--- osx_alias.bash
+++ osx_alias.bash
@@ -5,6 +5,7 @@ alias airdrop="pushd $(mktemp -d /tmp/opendrop-$(date +%Y-%m-%d).XXXXXX) ; echo 
 # enable opendrop reciving for 5 min
 alias airdrop_5min="pushd $(mktemp -d /tmp/opendrop-$(date +%Y-%m-%d).XXXXXX) ; echo Reciving directory : $(pwd) ; echo Command timeout : 5min ; gtimeout --preserve-status 5m opendrop receive ; popd"
 
+
 # paste the clip board to the command line
 alias pbp="pbpaste && echo"
 
@@ -14,9 +15,11 @@ alias remotehost_pbcopy="ssh login@hostname 'pbpaste' | pbcopy"
 # paste clipboard to remote system
 alias remotehost_pbpaste="pbpaste | ssh login@hostname 'cat - | pbcopy'"
 
+
 # enable / disable IPV6 (note you will need to set the names of other network interfaces you may have on your system - these are the defaults)
 alias ipv6_off="networksetup -setv6off Ethernet && networksetup -setv6off Wi-Fi"
 alias ipv6_auto="networksetup -setv6automatic Wi-Fi && networksetup -setv6automatic Ethernet"
+
 
 # network service order - 10.6 and earlier
 alias network_service_order_priority="networksetup -listnetworkserviceorder"
@@ -32,6 +35,7 @@ alias network_service_order_priority="networksetup -listnetworkserviceorder"
 
 # networksetup [-setnetworkserviceenabled networkservice on | off]
 
+
 # vpn connect / disconnect
 alias vpn_connect="/usr/bin/env osascript -e 'tell application \"System Events\"' -e 'tell current location of network preferences' -e 'set VPN to service \"VPN NAME\" -- your VPN name here' -e 'if exists VPN then connect VPN' -e 'end tell' -e 'end tell' > /dev/null"
 alias vpn_disconnect="/usr/bin/env osascript -e 'tell application \"System Events\"' -e 'tell current location of network preferences' -e 'set VPN to service \"VPN NAME\" -- your VPN name here' -e 'if exists VPN then disconnect VPN' -e 'end tell' -e 'end tell' > /dev/null"
@@ -40,6 +44,7 @@ alias vpn_disconnect="/usr/bin/env osascript -e 'tell application \"System Event
 # Screen Sharing App
 alias screen_sharing="open /System/Library/CoreServices/Screen\ Sharing.app"
 alias ovnc="open vnc://" # you can add a favoite into this - vnc://user@server:port to connect - if not it will just open the screen sharing application
+
 
 # apple airport command line utility
 alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
@@ -52,20 +57,26 @@ alias airport_power_off="networksetup -setairportpower AirPort off"
 alias airport_power_on="networksetup -setairportpower en1 on"
 alias airport_power_off="networksetup -setairportpower en1 off"
 
+
 # VLC Rotate Video
 alias vlc_rotate="/Applications/VLC.app/Contents/MacOS/VLC --vout-filter=transform --transform-type=180"
+
 
 # recursive md5
 alias checksum_directory_recursivly="find ./* -type f -exec md5 {} + | awk '{print \$1}' | sort | md5"
 
+
 # Date Month
 alias month_number="date +%m"
+
 
 # Start Screen Saver
 alias screensaver="/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine"
 
+
 # Battery Cycle Count
 alias battery_cylce_count="system_profiler SPPowerDataType | grep -i Cycle | awk -F \"Cycle count: \" '{print \$2}'"
+
 
 # Hardware Information
 alias hardware_information_SATA="system_profiler SPSerialATADataType"
@@ -73,16 +84,23 @@ alias hardware_information_USB="system_profiler SPUSBDataType"
 alias hardware_information_FIREWIRE="system_profiler SPFireWireDataType"
 alias hardware_information_THUNDERBOLT="system_profiler SPThunderboltDataType"
 
+
 # Keyspan
 alias keyspan_connect_and_log="screen -L /dev/tty.KeySerial1"
 alias keyspan_connect="screen /dev/tty.KeySerial1"
 
+
 # flush dns 10.5
-alias flush_dns_settings="dscacheutil -flushcache"
+alias flush_dns_cache="dscacheutil -flushcache"
+
+# fush dns 10.15 and later
+alias flush_dns_cache="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+
 
 # Safe Sleep
 alias safesleep_enabled='osascript -e "do shell script \"sudo sudo pmset -a hibernatemode 1\" with administrator privileges" 2>/dev/null'
 alias safesleep_disabled='osascript -e "do shell script \"sudo sudo pmset -a hibernatemode 0\" with administrator privileges" 2>/dev/null'
+
 
 # LS Setup
 alias ls="/bin/ls -G"
@@ -90,6 +108,7 @@ alias l="ls"
 alias ll="ls -l"
 alias la="ls -a"
 alias lla="ls -la"
+
 
 # screen saver time out value
 alias start_screensaver="open -a ScreenSaverEngine"
